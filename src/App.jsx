6f20a7cb6d76ddf4026f7067be5cf48c2f2f6db3
raman-webdev <chaudharyraman562@gmail.com>
2025-06-29 00:00:47 +0545

    Gemini API intrigated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { URL } from './constant';
 
 const App = () => {
+
+  const [question, setQuestion] = useState("");
+  const [result, setResult] = useState(undefined)
+  
+ const payload = {
+    "contents": [
+    {
+      "parts": [
+        {
+          "text": question
+        }
+      ]
+    }
+  ]
+}
+  const askQuestin = async () => {
+    let response = await fetch (URL + "AIzaSyBAC8-Y_UgVjyi5SeaUW_jkje7Iafbhrfw",{
+      method:"POST",
+      body:JSON.stringify(payload)
+    }) 
+    response = await response.json();
+    setResult(response.candidates[0].content.parts[0].text);
+  }
   return (
     <>
       <div className='grid grid-cols-5 text-white text-center h-screen'> 
@@ -12,19 +36,21 @@ const App = () => {
         {/* right-section */}
         <div className='col-span-4 p-10  '>
               <div className='container h-[550px] py-10'>
-                your answer is here
+              {result}
               </div>
-               <div className='bg-zinc-800-700 border-2 rounded-2xl border-zinc-700 h-16 w-[660px] m-auto  flex justify-between px-4'>
+               <div className='bg-zinc-800-700 border-2 rounded-2xl border-zinc-700  m-auto  flex justify-between px-4'>
+
                <input 
               type="text" 
-              placeholder='Ask here' 
-              
+              placeholder='Ask here'
+              className='w-full h-full outline-none ' 
+              onChange={(e) => setQuestion(e.target.value) }
               />
-              <button>Ask</button>
+              <button onClick={askQuestin}>Ask</button>
                </div>
 
           <div>
-
+           
           </div>
         </div>
       </div>  
